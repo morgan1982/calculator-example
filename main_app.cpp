99f282fec21b1e2bc4c4edd48892aa41130a3c90
entropy82 <froutopotos.alex@gmail.com>
2018-02-07 17:33:19 +0200

    add the main

--- main_app.cpp
+++ main_app.cpp
@@ -2,7 +2,41 @@
 
 
 using namespace std;
+
+class Calculator
+{
+    public:
+        int Calculate (int, int, char);
+};
+
+
 int main ()
 {
-    std::cout<< "hello world!"<<endl;
+    int x, y, result;
+    char oper;
+    cout<< "hello world!"<<endl;
+    cout<< "Please enter num1 operator num2: " <<endl;
+    cin >> x>>y>>oper;
+    Calculator c;
+    result = c.Calculate(x,y,oper);
+    cout << "Result is: "<<result<<endl;
+
+
+    cin.ignore();
+    cin.get();
+    return 0;
+}
+
+int Calculator::Calculate(int x, int y, char oper)
+{
+    switch (oper) {
+        case '+': return x+y;
+        case '-': return x-y;
+        case '*': return x*y;
+        case '/': if (y!=0) return x/y;
+        default:
+            cin >> x>>oper>>y;
+            Calculate(x,y,oper);
+
+    }
 }
